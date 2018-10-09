c1f02a3c2597aaa2fb88ab80b2a72b28fc71602c
Isco <islam.mb.94@gmail.com>
2018-10-09 13:20:06 -0500

    FANANDROID-188 Added back button

--- Assets/Scripts/SupportBackScript.cs
+++ Assets/Scripts/SupportBackScript.cs
@@ -1,8 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SupportBackScript: MonoBehaviour {
+
+    void Start()
+    {
+        transform.GetComponent<Button>().onClick.AddListener(HandleBackButton);
+    }
+
+    private void HandleBackButton()
+    {
+        Application.Quit();
+    }
 
     void FixedUpdate()
     {
